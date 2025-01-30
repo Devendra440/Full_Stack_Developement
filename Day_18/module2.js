df33bfc048336118c9e79feb2fa66417d3d9f95c
PINNAM DEVENDRA GUPTA <145124672+Devendra440@users.noreply.github.com>
2025-01-30 21:25:27 +0530

    Add files via upload

---
+++ Day_18/module2.js
@@ -0,0 +1,9 @@
+// import {msg} from "./script1.js";
+// import {greet} from "./script1.js";
+
+//  let abc = greet();
+//  console.log(abc);
+//  console.log(greet());
+import mru from "./script1.js";
+ 
+console.log(mru());
---
+++ Day_18/modules.js
@@ -0,0 +1,8 @@
+export let msg = "Namaskaram, bagunnara";
+ export function greet(){
+    return msg+"I am coding"
+}
+
+export default greet;msg;
+//create a form with username(!empty),eamil(@,!empty) and password (should be 6)  and confirm passwords (same,!empty),once you 
+//  click on submit button, a alert should be popped up on you screen "form submitted successfully" if and only if all the details are correct
