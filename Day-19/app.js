b9d32d965c21b41b3c8762ab3096ef73c9b23bce
PINNAM DEVENDRA GUPTA <145124672+Devendra440@users.noreply.github.com>
2025-01-31 18:46:57 +0530

    Add files via upload

---
+++ Day-19/app.js
@@ -0,0 +1,11 @@
+import college ,{X,user ,users ,obj} from './child.js';
+console.log(college);
+console.log(X);
+console.log(user);
+
+console.log(users);
+users.map(user=>{
+    console.log(user);
+})
+
+console.log(obj.company);
---
+++ Day-19/child.js
@@ -0,0 +1,42 @@
+// let x=10;
+// exportt default x;
+
+// let userDetail={
+//     name:"John",
+//     profession:"Software Developer"
+
+// }
+// export default userDetail;
+
+
+// ! named export
+
+// let x-10;
+// const user="John";
+// const users = ["John","Doe","Smith"];
+
+// const obj={
+//     name:"John",
+//     company:"Google"
+// }
+
+//! Named export 
+// export{
+//     x,
+//     user,
+//     users,
+//     obj
+// }
+
+export let X=10;
+export const user="John";
+export const users = ["John","Doe","Smith"];
+export const obj={
+    name:"John",
+    company:"Google"
+}
+
+let college="MallaReddy University";
+export default college;
+
+
